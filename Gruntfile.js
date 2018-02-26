1260826517bc7005946891739b1adc26aa31dea0
Jip <jhp.moors@gmail.com>
2018-02-26 10:45:19 +0100

    Remove extra tab

--- Gruntfile.js
+++ Gruntfile.js
@@ -38,7 +38,7 @@ module.exports = function ( grunt ) {
 	// Whenever the "test" task is run, first clean the "tmp" dir, then run the test
 	grunt.registerTask( "test", [ "clean", "nodeunit" ] );
 
-		// By default, lint and run all tests.
+	// By default, lint and run all tests.
 	grunt.registerTask( "default", [ "jshint", "test" ] );
 
 };
