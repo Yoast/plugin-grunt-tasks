17998825a2ffb8a74a1399b4682744bc7858f667
dutchmartin <martijn.groeneveldt@gmail.com>
2018-03-28 15:00:28 +0200

    fixed eslint

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,39 +1,35 @@
-"use strict";
-
-module.exports = function ( grunt ) {
-
+module.exports = function( grunt ) {
 	// Project configuration.
-	grunt.initConfig({
+	grunt.initConfig( {
 
 		eslint: {
 			options: {
-				configFile: '.eslintrc',
+				configFile: ".eslintrc",
 			},
-			target: ['*.js']
+			target: [ "*.js" ],
 		},
 
 		// Before generating any new files, remove any previously-created files.
 		clean: {
-			tests: [ "tmp" ]
+			tests: [ "tmp" ],
 		},
 
 		// Unit tests.
 		nodeunit: {
-			tests: [ "test/*test.js" ]
-		}
+			tests: [ "test/*test.js" ],
+		},
 
-	});
+	} );
 
 	// Actually load this plugin's task(s).
 	grunt.loadTasks( "tasks" );
 
-	// These plugins provide necessary tasks
+	// These plugins provide necessary tasks.
 	grunt.loadNpmTasks( "grunt-eslint" );
 	grunt.loadNpmTasks( "grunt-contrib-clean" );
 	grunt.loadNpmTasks( "grunt-contrib-nodeunit" );
 
-	// Whenever the "test" task is run,
-	// first clean the "tmp" dir, then run the test.
+	// Whenever the "test" task is run, first clean the "tmp" dir, then run the test.
 	grunt.registerTask( "test", [ "clean", "nodeunit" ] );
 
 	// By default, lint and run all tests.
