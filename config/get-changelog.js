72ad3f484a36a2da3892bfee633510a9315d7b37
noud-github <arnoud@yoast.com>
2021-02-19 10:28:54 +0100

    add premium to the job step 1

--- config/get-changelog.js
+++ config/get-changelog.js
@@ -1,12 +1,4 @@
 module.exports = {
-	options: {
-		useEditDistanceComapair: true,
-	},
-	"wordpress-seo": {
-		options: {
-			pluginSlug: "<%= pluginSlug %>",
-		},
-	},
 	"wordpress-seo-premium": {
 		options: {
 			pluginSlug: "<%= pluginSlug %>",
