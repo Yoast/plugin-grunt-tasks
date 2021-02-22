68730a2408fdbdeac415ee1d68df3b48fde31905
noud-github <arnoud@yoast.com>
2021-02-22 14:26:32 +0100

    make use of multitask part20

--- tasks/update-changelog-with-latest-pr-texts.js
+++ tasks/update-changelog-with-latest-pr-texts.js
@@ -56,6 +56,7 @@ class ChangelogBuilder {
 	parseChancelogLines(changelogIn){
 		this.grunt.verbose.writeln(changelogIn);
 		const parts = changelogIn.match(new RegExp( "\n[ a-zA-Z]+:(.|\\n)*?(?=(\n[ a-zA-Z]+:|\$))", "g" ));
+		console.log(parts)
 		if (parts) { 
 			parts.forEach(this.addLinesPerHeader.bind(this));
 		}
