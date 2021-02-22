1ff2d8c5e7dc7e37bbef307f88540bfd6ff2aa0f
noud-github <arnoud@yoast.com>
2021-02-22 12:45:08 +0100

    make use of multitask part11

--- config/update-changelog-with-latest-pr-texts.js
+++ config/update-changelog-with-latest-pr-texts.js
@@ -9,7 +9,7 @@ module.exports = {
 			releaseInChangelog: /[=] \d+\.\d+(\.\d+)? =/g,
 			matchChangelogHeader:  /[=]= Changelog ==/ig,
 			newHeadertemplate: "== Changelog ==\n\n" +"= " + "VERSIONNUMBER" + " =\nRelease Date: " + "DATESTRING"  + "\n",
-			matchCorrectHeader: "= " + "VERSIONNUMBER" + "(.|\\n)*?(?=(\\n\\n))",
+			matchCorrectHeader: "= " + "VERSIONNUMBER" + "(.|\\n)*?(?=(\\n\\w\+?:\\n))",
 			matchCorrectLines: "= " + "VERSIONNUMBER" + "(.|\\n)*?(?=(= \\d+[\.\\d]+ =|= Earlier versions =))",
 			matchCleanedChangelog: "= " + "VERSIONNUMBER" + "(.|\\n)*= Earlier versions =",
 			replaceCleanedChangelog: "= Earlier versions =",
