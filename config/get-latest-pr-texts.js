02bc440d2cfd7c6d623d7c0cfc060bce146fb815
noud-github <arnoud@yoast.com>
2021-02-25 09:31:21 +0100

    renamede get-changelog

---
+++ config/get-latest-pr-texts.js
@@ -0,0 +1,17 @@
+module.exports = {
+	"wordpress-seo": {
+		options: {
+			pluginSlug: "<%= pluginSlug %>",
+		},
+	},
+	"wordpress-seo-premium": {
+		options: {
+			pluginSlug: "<%= pluginSlug %>",
+		},
+	},
+	default: {
+		options: {
+			pluginSlug: "<%= pluginSlug %>",
+		},
+	},
+};
