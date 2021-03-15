6389c7db2c17c623ec849c3d3542fd53d1a96468
noud-github <arnoud@yoast.com>
2021-03-15 13:31:45 +0100

    code style

--- config/get-latest-pr-texts.js
+++ config/get-latest-pr-texts.js
@@ -1,5 +1,5 @@
 module.exports = {
-	default: {
+	"default": {
 		options: {
 			pluginSlug: "<%= pluginSlug %>",
 		},
--- config/update-changelog-with-latest-pr-texts.js
+++ config/update-changelog-with-latest-pr-texts.js
@@ -4,5 +4,5 @@ module.exports = {
 		useEditDistanceComapair: true,
 		pluginSlug: "<%= pluginSlug %>",
 		commitChangelog: true,
-	},	
+	},
 };
