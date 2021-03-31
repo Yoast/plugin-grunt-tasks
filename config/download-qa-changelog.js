d2476d2b9bc0be5bbfeccbea2b362a35a7bf0767
noud-github <arnoud@yoast.com>
2021-03-31 09:08:37 +0200

    add download function

---
+++ config/download-qa-changelog.js
@@ -0,0 +1,11 @@
+/* eslint-disable max-len */
+/* eslint-disable no-useless-escape */
+/* eslint-disable no-useless-concat */
+// Custom task
+module.exports = {
+	"wordpress-seo-premium": {
+		options: {
+			pluginSlug: "<%= pluginSlug %>",
+		},
+	},
+};
